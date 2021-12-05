100eb80120a88bda8549b8cf68cb826b5ba5a983
devnull850 <dev.null850@gmail.com>
2021-12-05 10:37:36 -0500

    Add tokens -dev0

---
+++ token.c
@@ -0,0 +1,29 @@
+/**
+    Tokens based on operations of LC3 ISA
+
+    Note: This is a subset of valid C tokens
+*/
+const char ADD[] 		= "+";
+const char AND[] 		= "&";
+const char IF[] 		= "if";
+const char ELSE[] 		= "else";
+const char FOR[] 		= "for";
+const char WHILE[] 		= "while";
+const char DO[] 		= "do";
+const char NOT[] 		= "!";
+const char RETURN[] 		= "return";
+const char ASSIGN[] 		= "=";
+const char EQUAL[] 		= "==";
+const char GREATER[] 		= ">";
+const char LESS[] 		= "<";
+const char GREATEREQUAL[]	= ">=";
+const char LESSEQUAL[] 		= "<=";
+const char NOTEQUAL[] 		= "!=";
+const char SEMICOLON[] 		= ";";
+const char COMMA[] 		= ",";
+const char LPAREN[] 		= "(";
+const char LBRACE[] 		= "{";
+const char RPAREN[] 		= ")";
+const char RBRACE[] 		= "}";
+const char BREAK[] 		= "break";
+const char CONTINUE[]		= "continue";
